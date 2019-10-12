a890ee01e661991c49f9d160a8ee88991cc3f8dd
Ryan Jasinski <jasinski.17@gmail.com>
2019-10-12 12:54:49 -0400

    first sections working

--- src/bakery.js
+++ src/bakery.js
@@ -1,4 +1,26 @@
 // Up to you to decide what goes here based on the tests...
-const Bakery = null;
+class Bakery {
+  constructor(name = 'Eliots Bakery') {
+    this.name = name;
+  }
+  bake(item) {
+    let bakedItem = {
+      type: item,
+      consume() {
+        this.counter++;
+        if (this.counter <= 1) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+      counter: 0,
+      price: 0,
+    };
+    return bakedItem;
+  }
+
+  price(item, price) {}
+}
 
 module.exports = Bakery;
